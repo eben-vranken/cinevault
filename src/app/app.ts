f06e5feb9b7a099373e9b2f3fc3d9c60b3e97557
eben-vranken <vranken.eben@outlook.com>
2026-02-16 13:11:33 +0100

    Boilerplate movieposter component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { MoviePoster } from '../components/movieposter/movieposter';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [MoviePoster],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/components/movieposter/movieposter.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "movieposter",
+  templateUrl: "./movieposter.html",
+})
+export class MoviePoster {
+
+}
