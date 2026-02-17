4f3208ec78e97ce2455ac365e2e2f7c5b614f68a
eben-vranken <vranken.eben@outlook.com>
2026-02-17 13:43:41 +0100

    mobile responsiveness

--- src/components/navbar/navbar.ts
+++ src/components/navbar/navbar.ts
@@ -1,11 +1,20 @@
-import { Component } from "@angular/core";
-import { RouterLink, RouterLinkActive } from "@angular/router";
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
-    selector: 'navbar',
-    templateUrl: './navbar.html',
-    imports: [RouterLink, RouterLinkActive]
+  selector: 'navbar',
+  templateUrl: './navbar.html',
+  imports: [RouterLink, RouterLinkActive, CommonModule],
 })
 export class Navbar {
+  isMenuOpen = false;
 
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu() {
+    this.isMenuOpen = false;
+  }
 }
